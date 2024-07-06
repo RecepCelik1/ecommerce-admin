13344ce793e87ed6c23e36e9e9d0a426baa32a94
RecepCelik1 <recep.celik@std.yildiz.edu.tr>
2024-07-06 16:03:08 +0300

    final

---
+++ app/(root)/(routes)/page.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const SetupPage = () => {
+
+    const onOPen = useStoreModal((state) => state.onOpen);
+    const isOpen = useStoreModal((state) => state.isOpen);
+
+    useEffect(() => {
+        if(!isOpen) {
+            onOPen();
+        }
+    },[isOpen , onOPen]);
+
+    return null;
+  }
+  
+export default SetupPage;
